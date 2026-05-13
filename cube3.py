6109817aeb9d73f3952dcab30102045336c76529
isabel <novaverloren@javerianacali.edu.co>
2026-05-13 13:44:50 -0500

    mejoras en eiciencia

--- cube3.py
+++ cube3.py
@@ -16,42 +16,45 @@ def verBit(n, pos):
 def swap(n, pos):
     return n ^ (1 << 5 - pos)
 
-def move(d, rc, pos, gm, g, R, C):
+def move(d, rc, R, C):
     #0 = north, 1 = east, 2 = south, 3 = west
-    op = [2, 3, 0, 1, 5, 4]
+    dire = ((-1, 0), (0, 1), (1, 0), (0, -1))
+    
+    nrc = (rc[0] + dire[d][0], rc[1] + dire[d][1])
+    flag = nrc[0] >= 0 and nrc[0] < R and nrc[1] >= 0 and nrc[1] < C
+    return (flag, nrc)
+
+def roll(d, nrc, pos):
+    #0 = north, 1 = east, 2 = south, 3 = west
+    op = (2, 3, 0, 1, 5, 4)
     
     if d == 0:
-        ans = (pos[1], op[pos[0]], pos[2])
-        flag = rc[0] - 1 >= 0
-        nrc = (rc[0] - 1, rc[1])
+        npos = (pos[1], op[pos[0]], pos[2])
     elif d == 1:
-        ans = (pos[2], pos[1], op[pos[0]])
-        flag = rc[1] + 1 < C
-        nrc = (rc[0], rc[1] + 1)
+        npos = (pos[2], pos[1], op[pos[0]])
     elif d == 2:
-        ans = (op[pos[1]], pos[0], pos[2])
-        flag = rc[0] + 1 < R
-        nrc = (rc[0] + 1, rc[1])
+        npos = (op[pos[1]], pos[0], pos[2])
     else:
-        ans = (op[pos[2]], pos[1], pos[0])
-        flag = rc[1] - 1 >= 0
-        nrc = (rc[0], rc[1] - 1)
-    
-    if flag and nrc in gm:
-        if not verBit(g, ans[0]):
-            g = swap(g, ans[0])
-            gm.remove(nrc)
-    elif flag:
-        if verBit(g, ans[0]):
-            g = swap(g, ans[0])
-            gm.add(nrc)
+        npos = (op[pos[2]], pos[1], pos[0])
+    return npos
 
-    return (flag, nrc, ans, g, gm)
+def checkGold(nrc, npos, g, gm):
+    if nrc in gm:
+        if not verBit(g, npos[0]):
+            g = swap(g, npos[0])
+            gm = gm.copy()
+            gm.remove(nrc)
+    else:
+        if verBit(g, npos[0]):
+            g = swap(g, npos[0])
+            gm = gm.copy()
+            gm.add(nrc)
+    return (g, gm)
 
 def dijkstra(rc, gold, R, C, A, B):
     #estado: ((row, col), (floor, north, east), g, goldMap)
     
-    act = (0, rc, (0, 1, 4), 0, gold.copy())
+    act = (0, rc, (0, 1, 4), 0, gold)
     vis = {(rc, (0, 1, 4), 0, frozenset(gold)): 0}
     flag = False
     cost = 0
@@ -61,24 +64,27 @@ def dijkstra(rc, gold, R, C, A, B):
     while not flag and len(q) > 0:
       c, rc, pos, g, gm = heappop(q)
       i = 0
-      while vis[(rc, pos, g, frozenset(gm))] == c and not flag and i < 4:
-        nflag, nrc, npos, ng, ngm = move(i, rc, pos, gm.copy(), g, R, C)
-        ngm = ngm.copy()
+      while not flag and i < 4 and vis[(rc, pos, g, frozenset(gm))] == c:
+        nflag, nrc = move(i, rc, R, C)
         ac = A
-        if ng > g:
-            ac = B
             
         if nflag:
+            npos = roll(i, nrc, pos)
+            ng, ngm = checkGold(nrc, npos, g, gm)
             fngm = frozenset(ngm)
-            if ((nrc, npos, ng, fngm) not in vis or vis[(nrc, npos, ng, fngm)] > c + ac):
-                vis[(nrc, npos, ng, fngm)] = c + ac
+            if ng > g:
+                ac = B
+            ns = (nrc, npos, ng, fngm)
+            cost = c + ac
             
+            if ns not in vis or vis[ns] > cost:
                 #63 = 111111
-                if ng == 63:
-                    flag = True
-                    cost = c + ac
-                else:
-                    heappush(q, (c + ac, nrc, npos, ng, ngm))
+                
+                flag = ng == 63
+                if not flag:
+                    vis[ns] = cost
+                    heappush(q, (cost, nrc, npos, ng, ngm))
+                    
         i += 1
     return (flag, cost)
         
@@ -88,14 +94,20 @@ def main():
     for i in range(T):
         R, C, A, B = list(map(int, stdin.readline().split()))
         gold = set()
+        rc = (-1, -1)
+        flag = False
+        
         for j in range(R):
             aux = stdin.readline()
-            for k in range(C):
+            k = 0
+            while not flag and k < C:
                 if aux[k] == 'G':
                     gold.add((j, k))
                 elif aux[k] == 'S':
                     rc = (j, k)
-
+                flag = len(gold) == 6 and rc != (-1, -1)
+                k += 1
+                
         flag, cost = dijkstra(rc, gold, R, C, A, B)
         
         if flag:
