84ce08fcf481a8aa7284e17a13a0d2683a218110
isabel <novaverloren@javerianacali.edu.co>
2026-05-14 10:50:07 -0500

    corrección cube 4 tupla de mascaras, avances cube 5 mascara para el mapa

--- cube5.py
+++ cube5.py
@@ -10,11 +10,23 @@ Código de estudiante: 8977586,
 from sys import stdin
 from heapq import heappop, heappush
 
-def verBit(n, pos):
-    return bool(n & (1 << 5 - pos))
+def verBit(n, pos, bits):
+    return bool(n & (1 << bits - pos))
 
-def swap(n, pos):
-    return n ^ (1 << 5 - pos)
+def swap(n, pos, bits):
+    print(n, pos, bits)
+    print(bin(n))
+    print(bin(1 << bits - pos))
+    print(bin(n ^ (1 << bits - pos)))
+    return n ^ (1 << bits - pos)
+
+def fn(r, c, C):
+    return r * C + c
+
+def f(k, C):
+    r = floor(k/C)
+    c = k - r * C
+    return (r, c)
 
 def move(d, rc, R, C):
     #0 = north, 1 = east, 2 = south, 3 = west
@@ -24,7 +36,6 @@ def move(d, rc, R, C):
     flag = nrc[0] >= 0 and nrc[0] < R and nrc[1] >= 0 and nrc[1] < C
     return (flag, nrc)
 
-"""
 def move2(d, rc, R, C):
     #0 = north, 1 = east, 2 = south, 3 = west
     dire = ((-1, 0), (0, 1), (1, 0), (0, -1))
@@ -39,7 +50,6 @@ def move2(d, rc, R, C):
     else:
         flag = nrc[1] >= 0
     return (flag, nrc)
-"""
 
 def roll(d, nrc, pos):
     #0 = north, 1 = east, 2 = south, 3 = west
@@ -55,24 +65,23 @@ def roll(d, nrc, pos):
         npos = (op[pos[2]], pos[1], pos[0])
     return npos
 
-def checkGold(nrc, npos, g, gm):
-    if nrc in gm:
-        if not verBit(g, npos[0]):
-            g = swap(g, npos[0])
-            gm = gm.copy()
-            gm.remove(nrc)
+def checkGold(nrc, npos, g, gm, R, C):
+    flag = verBit(g, npos[0], 5)
+    if verBit(gm, fn(nrc[0], nrc[1], C), R * C - 1):
+        if not flag:
+            g = swap(g, npos[0], 5)
+            gm = swap(gm, fn(nrc[0], nrc[1], C), R * C - 1)
     else:
-        if verBit(g, npos[0]):
-            g = swap(g, npos[0])
-            gm = gm.copy()
-            gm.add(nrc)
+        if flag:
+            g = swap(g, npos[0], 5)
+            gm = swap(gm, fn(nrc[0], nrc[1], C), R * C - 1)
     return (g, gm)
 
 def dijkstra(rc, gold, R, C, A, B):
     #estado: ((row, col), (floor, north, east), g, goldMap)
     
     act = (0, rc, (0, 1, 4), 0, gold)
-    vis = {(rc, (0, 1, 4), 0, frozenset(gold)): 0}
+    vis = {(rc, (0, 1, 4), 0, gold): 0}
     flag = False
     cost = 0
     q = []
@@ -82,17 +91,16 @@ def dijkstra(rc, gold, R, C, A, B):
       c, rc, pos, g, gm = heappop(q)
       i = 0
       
-      while not flag and i < 4 and vis[(rc, pos, g, frozenset(gm))] == c:
+      while not flag and i < 4 and vis[(rc, pos, g, gm)] == c:
         nflag, nrc = move(i, rc, R, C)
         ac = A
             
         if nflag:
             npos = roll(i, nrc, pos)
-            ng, ngm = checkGold(nrc, npos, g, gm)
-            fngm = frozenset(ngm)
+            ng, ngm = checkGold(nrc, npos, g, gm, R, C)
             if ng > g:
                 ac = B
-            ns = (nrc, npos, ng, fngm)
+            ns = (nrc, npos, ng, ngm)
             cost = c + ac
             
             if ns not in vis or vis[ns] > cost:
@@ -102,7 +110,7 @@ def dijkstra(rc, gold, R, C, A, B):
                     flag = True
                 else:
                     vis[ns] = cost
-                    heappush(q, (cost, nrc, npos, ng, ngm))   
+                    heappush(q, ns)  
         i += 1
         
     return (flag, cost)
@@ -112,7 +120,8 @@ def main():
     T = int(stdin.readline())
     for i in range(T):
         R, C, A, B = list(map(int, stdin.readline().split()))
-        gold = set()
+        gold = 0
+        gCount = 0
         rc = (-1, -1)
         flag = False
         
@@ -121,12 +130,13 @@ def main():
             k = 0
             while not flag and k < C:
                 if aux[k] == 'G':
-                    gold.add((j, k))
+                    gold = swap(gold, fn(j, k, C), C - 1)
+                    gCount += 1
                 elif aux[k] == 'S':
                     rc = (j, k)
-                flag = len(gold) == 6 and rc != (-1, -1)
+                flag = gCount == 6 and rc != (-1, -1)
                 k += 1
-                
+        print("uwu")
         flag, cost = dijkstra(rc, gold, R, C, A, B)
         
         if flag:
