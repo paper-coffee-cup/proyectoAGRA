21e61ce2e250e70e9419bdd708e885d07dde4b7c
isabel <novaverloren@javerianacali.edu.co>
2026-06-03 00:14:21 -0500

    mejora en tiempo, se precalcula

--- cube25.cpp
+++ cube25.cpp
@@ -18,21 +18,20 @@ int dire[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 int fn[8][8];
 pair<int, int> f[64];
 
-// i: {north, east, south, west}
-vector<vector<int>> roll = {{0, 0, 0, 0}, {1, 32, 1, 8}, {2, 8, 2, 32}, {3, 40, 3, 40}, {32, 4, 8, 4},
-			    {33, 36, 9, 12}, {34, 12, 10, 36}, {35, 44, 11, 44}, {4, 1, 16, 2}, {5, 33, 17, 10},
-			    {6, 9, 18, 34}, {7, 41, 19, 42}, {36, 5, 24, 6}, {37, 37, 25, 14}, {38, 13, 26, 38},
-			    {39, 45, 27, 46}, {8, 16, 32, 16}, {9, 48, 33, 24}, {10, 24, 34, 48}, {11, 56, 35, 56},
-			    {40, 20, 40, 20}, {41, 52, 41, 28}, {42, 28, 42, 52}, {43, 60, 43, 60}, {12, 17, 48, 18},
-			    {13, 49, 49, 26}, {14, 25, 50, 50}, {15, 57, 51, 58}, {44, 21, 56, 22}, {45, 53, 57, 30},
-			    {46, 29, 58, 54}, {47, 61, 59, 62}, {16, 2, 4, 1}, {17, 34, 5, 9}, {18, 10, 6, 33},
-			    {19, 42, 7, 41}, {48, 6, 12, 5}, {49, 38, 13, 13}, {50, 14, 14, 37}, {51, 46, 15, 45},
-			    {20, 3, 20, 3}, {21, 35, 21, 11}, {22, 11, 22, 35}, {23, 43, 23, 43}, {52, 7, 28, 7},
-			    {53, 39, 29, 15}, {54, 15, 30, 39}, {55, 47, 31, 47}, {24, 18, 36, 17}, {25, 50, 37, 25},
-			    {26, 26, 38, 49}, {27, 58, 39, 57}, {56, 22, 44, 21}, {57, 54, 45, 29}, {58, 30, 46, 53},
-			    {59, 62, 47, 61}, {28, 19, 52, 19}, {29, 51, 53, 27}, {30, 27, 54, 51}, {31, 59, 55, 59},
-			    {60, 23, 60, 23}, {61, 55, 61, 31}, {62, 31, 62, 55}, {63, 63, 63, 63}};
+int moves[64][4];
 
+//i: {north, east, south, west}
+int roll[64][4] = {{0, 0, 0, 0}, {1, 32, 1, 8}, {2, 8, 2, 32}, {3, 40, 3, 40}, {32, 4, 8, 4}, {33, 36, 9, 12},
+		   {34, 12, 10, 36}, {35, 44, 11, 44}, {4, 1, 16, 2}, {5, 33, 17, 10}, {6, 9, 18, 34}, {7, 41, 19, 42},
+		   {36, 5, 24, 6}, {37, 37, 25, 14}, {38, 13, 26, 38}, {39, 45, 27, 46}, {8, 16, 32, 16}, {9, 48, 33, 24},
+		   {10, 24, 34, 48}, {11, 56, 35, 56}, {40, 20, 40, 20}, {41, 52, 41, 28}, {42, 28, 42, 52}, {43, 60, 43, 60},
+		   {12, 17, 48, 18}, {13, 49, 49, 26}, {14, 25, 50, 50}, {15, 57, 51, 58}, {44, 21, 56, 22}, {45, 53, 57, 30},
+		   {46, 29, 58, 54}, {47, 61, 59, 62}, {16, 2, 4, 1}, {17, 34, 5, 9}, {18, 10, 6, 33}, {19, 42, 7, 41},
+		   {48, 6, 12, 5}, {49, 38, 13, 13}, {50, 14, 14, 37}, {51, 46, 15, 45}, {20, 3, 20, 3}, {21, 35, 21, 11},
+		   {22, 11, 22, 35}, {23, 43, 23, 43}, {52, 7, 28, 7}, {53, 39, 29, 15}, {54, 15, 30, 39}, {55, 47, 31, 47},
+		   {24, 18, 36, 17}, {25, 50, 37, 25}, {26, 26, 38, 49}, {27, 58, 39, 57}, {56, 22, 44, 21}, {57, 54, 45, 29},
+		   {58, 30, 46, 53}, {59, 62, 47, 61}, {28, 19, 52, 19}, {29, 51, 53, 27}, {30, 27, 54, 51}, {31, 59, 55, 59},
+		   {60, 23, 60, 23}, {61, 55, 61, 31}, {62, 31, 62, 55}, {63, 63, 63, 63}};
 
 class State {
   // (cost, rowCol, cube, goldMap)
@@ -102,7 +101,7 @@ inline pair<int, int> genF(int k, int C) {
   return make_pair(r, c);
 }
 
-inline pair<bool, pair<int, int>> move(int d, int rc, int R, int C) {
+inline pair<bool, pair<int, int>> genMove(int d, int rc, int R, int C) {
   pair<int, int> prc = f[rc];
   int nr = prc.first + dire[d][0];
   int nc = prc.second + dire[d][1];
@@ -136,9 +135,9 @@ pair<bool, unsigned long long> dijkstra(int rc, unsigned long long gold, int R, 
   vector<vector<unordered_map<unsigned long long, int>>> vis(R * C, vector<unordered_map<unsigned long long, int>>(64));
   priority_queue<State, vector<State>, greater<State>> q;
   pair<bool, pair<int, unsigned long long>> ncgm;
-  pair<bool, pair<int, int>> pnrc;
+  pair<int, int> prc;
   
-  int co, c, nrc, nr, nc, nC, cost, ac, sz = R * C - 1;
+  int co, c, nrc, nc, cost, ac, sz = R * C - 1;
   unsigned long long gm, ngm;
   
   bool flag = false;
@@ -161,12 +160,10 @@ pair<bool, unsigned long long> dijkstra(int rc, unsigned long long gold, int R, 
       
     } else if (vis[rc][c][gm] == co){
       for (int i = 0; i < 4; ++i) {
-	pnrc = move(i, rc, R, C);
+	nrc = moves[rc][i];
                   
-	if (pnrc.first) {
-	  nr = (pnrc.second).first;
-	  nC = (pnrc.second).second;
-	  nrc = fn[nr][nC];
+	if (nrc != -1) {
+	  prc = f[nrc];
 	  ac = A;
 	
 	  nc = roll[c][i];
@@ -189,9 +186,20 @@ pair<bool, unsigned long long> dijkstra(int rc, unsigned long long gold, int R, 
   return make_pair(flag, co);
 }
 
+void printMatrix(int arr[64][4]) {
+    for (int i = 0; i < 64; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main() {
-  int T, R, C, A, B, rc, gCount;
+  int T, R, C, A, B, rc, gCount, fAux, lR = -1, lC = -1;
   pair<bool, unsigned long long> res;
+  pair<bool, pair<int, int>> moveAux;
+  pair<int, int> moveAuxF;
   unsigned long long gold = 0;
   bool flag = false;
   char aux[9];
@@ -215,11 +223,26 @@ int main() {
 	flag = gCount == 6 && rc != -1;
       }
     }
-        
-    for (int j = 0; j < R; ++j) {
-      for (int k = 0; k < C; ++k) {
-	fn[j][k] = genFn(j, k, C);
-	f[fn[j][k]] = make_pair(j, k);
+
+    if (lR != R || lC != C) {
+      for (int j = 0; j < R; ++j) {
+	for (int k = 0; k < C; ++k) {
+	  fAux = genFn(j, k, C);
+	  fn[j][k] = fAux;
+	  f[fAux] = make_pair(j, k);
+	}
+      }
+
+      for (int j = 0; j < R * C; ++j) {
+	for (int k = 0; k < 4; ++k) {
+	  moveAux = genMove(k, j, R, C);
+	  moveAuxF = moveAux.second;
+	
+	  if (moveAux.first)
+	    moves[j][k] = fn[moveAuxF.first][moveAuxF.second];
+	  else
+	    moves[j][k] = -1;
+	}
       }
     }
     
@@ -232,4 +255,3 @@ int main() {
   } 
   return 0;
 }
-
